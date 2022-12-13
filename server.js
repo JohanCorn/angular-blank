8209e7faa30eb5f2b49faf1b28283032a2c93955
Szabó Erhard Pál <erhard.schneider.1337@gmail.com>
2022-12-13 23:33:01 +0100

    Update for Heroku

---
+++ server.js
@@ -0,0 +1,5 @@
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname + '/dist/angular-blank'));
+app.get('/*', (req, resp) => resp.sendFile(__dirname + '/dist/angular-blank/index.html'));
+app.listen(process.env.PORT || 8080);
